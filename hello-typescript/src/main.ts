458f2bcc13c6313eeb12481b43cdc0f6f3471556
asma-abbasi <asmaabbasi7712@gmail.com>
2025-02-11 10:40:27 +0330

    [ADD] add sort entities algorithm

--- hello-typescript/src/main.ts
+++ hello-typescript/src/main.ts
@@ -1,5 +1,7 @@
 import { reverseArray } from "./task-2/reverse-array";
 import { countNumbers } from "./task-2/count-numbers";
+import { Entity } from "./task-2/entities";
+import { sortEntities } from "./task-2/sort-entities";
 
 function testReverseArray(): void {
   const items = [1, 2, 3];
@@ -13,9 +15,26 @@ function testCountNumber(): void {
   console.log(count);
 }
 
+function testSortEntities(): void {
+  const entities: Entity[] = [
+    {
+      createAt: "2025-02-11T00:00:00.000Z",
+      name: "asma",
+    },
+    {
+      createAt: "2025-01-31T00:00:00.000Z",
+      modifiedAt: "2025-02-10T00:00:00.000Z",
+    },
+  ];
+
+  const sortedEntities = sortEntities(entities);
+  console.log(sortedEntities);
+}
+
 function main(): void {
   testReverseArray();
   testCountNumber();
+  testSortEntities();
 }
 
 main();
